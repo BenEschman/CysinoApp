04c3dee9123771ed1a796a11f0388850eede41c9
BenEschman <beschman2005@gmail.com>
2024-09-12 16:31:09 -0500

    significant changes per request

--- Experiments/BenEschman/demos/tutorials/hellopeople/src/main/java/coms309/WelcomeController.java
+++ Experiments/BenEschman/demos/tutorials/hellopeople/src/main/java/coms309/WelcomeController.java
@@ -3,6 +3,8 @@ package coms309;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.*;
+
 /**
  * Simple Hello World Controller to display the string returned
  *
@@ -15,5 +17,13 @@ class WelcomeController {
     @GetMapping("/")
     public String welcome() {
         return "Hello and welcome to COMS 309";
+    }
+
+    @GetMapping("/secret")
+    public String secret(){return "you found a secret";}
+
+    @GetMapping("/secret2")
+    public String displayImage() {
+        return "<html><body><img src='dog.jpg' /></body></html>";
     }
 }
