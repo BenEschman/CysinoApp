ca2bbf4ffc571373938beea25b9b9e07769d21fb
BenEschman <beschman2005@gmail.com>
2024-09-16 18:49:27 -0500

    Backend outline

---
+++ Backend/Cycino/src/main/java/coms309/Cycino/CycinoApplication.java
@@ -0,0 +1,13 @@
+package coms309.Cycino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CycinoApplication.class, args);
+	}
+
+}
---
+++ Backend/Cycino/src/main/java/coms309/Cycino/users/User.java
@@ -0,0 +1,76 @@
+package coms309.Cycino.users;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private String id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String password;
+    private String role;
+
+    public User(String id, String username, String password){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+    public User(String id, String username, String password, String first, String last){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = first;
+        this.lastName = last;
+    }
+    public User(String id, String username, String password, String first, String last, String phoneNumber){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = first;
+        this.lastName = last;
+        this.phoneNumber = phoneNumber;
+    }
+    public User(String id, String username, String password, String first, String last, String phoneNumber, String role){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = first;
+        this.lastName = last;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    public String getId(){
+        return id;
+    }
+    public String[] getLogin(){
+        return new String[]{username, password};
+    }
+    public String[] getContact(){
+        return new String[]{firstName, lastName, phoneNumber};
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public void updateLogin(String[] login){
+        this.username = login[0];
+        this.password = login[1];
+    }
+
+    public void updateContact(String[] contact){
+        this.firstName = contact[0];
+        this.lastName = contact[1];
+        this.phoneNumber = contact[2];
+    }
+
+    public void updateRole(String role){
+        this.role = role;
+    }
+}
