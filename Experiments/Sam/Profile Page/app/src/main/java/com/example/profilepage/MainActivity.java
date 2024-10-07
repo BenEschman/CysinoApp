0f98bb55356f70bdcc47ee2633c990154f7f5e93
Sam Craft <craftsam@iastate.edu>
2024-10-07 14:29:31 -0500

    Created profile page with no backend interaction

--- Experiments/Sam/Profile Page/app/src/main/java/com/example/profilepage/MainActivity.java
+++ Experiments/Sam/Profile Page/app/src/main/java/com/example/profilepage/MainActivity.java
@@ -19,8 +19,12 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,8 +34,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView profileText;
     private EditText firstName;
     private EditText lastName;
+    private EditText phoneNumber;
     private Button submit;
     private Button view;
+    private Button leaderboard;
     RequestQueue requestQueue;
 
     @Override
@@ -43,14 +49,20 @@ public class MainActivity extends AppCompatActivity {
         profileText = findViewById(R.id.profileText);
         firstName = findViewById(R.id.firstName);
         lastName = findViewById(R.id.lastName);
+        phoneNumber = findViewById(R.id.editTextPhone);
         submit = findViewById(R.id.submitButton);
         view = findViewById(R.id.viewProfile);
+        leaderboard = findViewById(R.id.button_leaderboard);
 
         profileText.setPadding(0,130,0,0);
         profileText.setTextSize(30);
         profileText.setText("Profile Page");
 
         submit.setText("Save Profile");
+        submit.setBackgroundColor(0xFFFFFF00);
+        submit.setTextColor(0xFF000000);
+        view.setBackgroundColor(0xFFFFFF00);
+        view.setTextColor(0xFF000000);
 
         requestQueue = Volley.newRequestQueue(MainActivity.this);
 
@@ -60,8 +72,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String fNameStr = firstName.getText().toString();
                 String lNameStr = lastName.getText().toString();
+                String phoneNum = phoneNumber.getText().toString();
 
-                    pushData(fNameStr,lNameStr);
+                    pushData(fNameStr,lNameStr,phoneNum);
                     System.out.println(fNameStr + lNameStr);
 
 
@@ -76,39 +89,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        leaderboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LeaderboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
 
-    private void pushData(final String fName, final String lName) {
-        String url = "https://10c011fe-3b08-4ae2-96a7-71049edb34ae.mock.pstmn.io/postData";
-        StringRequest sReq = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Toast.makeText(MainActivity.this, "Data Sent" + response, Toast.LENGTH_SHORT).show();
+    private void pushData(final String fName, final String lName, final String phoneNum) {
+        //String url = "https://10c011fe-3b08-4ae2-96a7-71049edb34ae.mock.pstmn.io/putData";
+        //String url = "http://coms-3090-052.class.las.iastate.edu:8080/users/create";
+        String url = "10.90.75.20:8080/users/create";
 
+        JSONObject postData = new JSONObject();
+
+        try {
+            postData.put("firstName",fName);
+            postData.put("lastName",lName);
+            postData.put("phoneNumber",phoneNum);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(getApplicationContext(), "Response: "+response, Toast.LENGTH_LONG).show();
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, "Data Failed", Toast.LENGTH_SHORT).show();
+                error.printStackTrace();
+                Toast.makeText(getApplicationContext(),"FAIL", Toast.LENGTH_LONG).show();
             }
-        }) {
-            @Nullable
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("fName",fName);
-                params.put("lName",lName);
-                System.out.println(params.toString());
-                return params;
-            }
-            public Map<String, String> getHeaders() {
-                Map<String, String> headers = new HashMap<>();
-                headers.put("Content-Type", "application/x-www-form-urlencoded");
-                return headers;
-            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+
         };
-        requestQueue.add(sReq);
-    }
+
 }
