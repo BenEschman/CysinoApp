13781f78b0b8e4ff37b1d44e52803bd0eaac4132
filfor <filfor@iastate.edu>
2024-10-09 15:55:04 -0500

    updated response type to json from boolean for usercontroller and follocontroller

--- Backend/Cycino/src/main/java/coms309/Cycino/follow/FollowController.java
+++ Backend/Cycino/src/main/java/coms309/Cycino/follow/FollowController.java
@@ -6,7 +6,9 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class FollowController {
@@ -21,12 +23,24 @@ public class FollowController {
     }
 
     @PostMapping("/users/{uid}/follow")
-    public boolean addToFollowList(@RequestBody Follow follow, @PathVariable long uid){
-        return followService.addToFollowList(follow, uid);
+    public Map<String, Object> addToFollowList(@RequestBody Follow follow, @PathVariable long uid){
+        Map<String, Object> result = new HashMap<>();
+        if (followService.addToFollowList(follow, uid)){
+            result.put("status", "200 OK");
+        } else {
+            result.put("status", "400 Bad Request");
+        }
+        return result;
     }
 
     @DeleteMapping("/users/{uid}/unfollow")
-    public boolean removeFromFollowList(@RequestBody Follow follow, @PathVariable long uid){
-        return followService.removeFromFollowList(follow, uid);
+    public Map<String, Object> removeFromFollowList(@RequestBody Follow follow, @PathVariable long uid){
+        Map<String, Object> result = new HashMap<>();
+        if (followService.removeFromFollowList(follow, uid)){
+            result.put("status", "200 OK");
+        } else {
+            result.put("status", "400 Bad Request");
+        }
+        return result;
     }
 }
