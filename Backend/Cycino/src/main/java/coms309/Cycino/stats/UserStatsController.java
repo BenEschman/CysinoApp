95a2985190b660d60b09fd4a185022b1ccc1d342
BenEschman <beschman2005@gmail.com>
2024-10-10 15:28:41 -0500

    Added sorted userStats

--- Backend/Cycino/src/main/java/coms309/Cycino/stats/UserStatsController.java
+++ Backend/Cycino/src/main/java/coms309/Cycino/stats/UserStatsController.java
@@ -6,10 +6,7 @@ import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class UserStatsController {
@@ -31,6 +28,7 @@ public class UserStatsController {
             if(id.equalsIgnoreCase(game))
                 userStats.add(u);
         }
+        userStats.sort((o1, o2) -> (int) ((o1.getNet() - o2.getNet()) / (Math.abs(o1.getNet() - o2.getNet()))));
         return userStats;
     }
 
