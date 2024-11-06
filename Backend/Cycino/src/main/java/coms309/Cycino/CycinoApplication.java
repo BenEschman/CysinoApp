bce37fdec9f58933560803ef2777e16891c3711c
BenEschman <beschman2005@gmail.com>
2024-11-06 00:31:31 -0600

    fixes

--- Backend/Cycino/src/main/java/coms309/Cycino/CycinoApplication.java
+++ Backend/Cycino/src/main/java/coms309/Cycino/CycinoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.demo.websocket"})
+@ComponentScan(basePackages = {"coms"})
 public class CycinoApplication {
 
 
