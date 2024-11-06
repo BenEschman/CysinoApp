76e4168f7d10e696265add87aec0f1f66606ae12
filfor <filfor@iastate.edu>
2024-11-06 00:04:14 -0600

    trying to fix websockets

--- Backend/Cycino/src/main/java/coms309/Cycino/CycinoApplication.java
+++ Backend/Cycino/src/main/java/coms309/Cycino/CycinoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"coms309.Cycino.directMessaging"})
+//@ComponentScan(basePackages = {"coms309.Cycino.directMessaging"})
 public class CycinoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CycinoApplication.class, args);
