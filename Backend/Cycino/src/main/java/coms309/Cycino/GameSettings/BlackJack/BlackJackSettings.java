da7338d7f82f3e59161e6ac7a827180630262f0b
filfor <filfor@iastate.edu>
2024-11-07 14:02:38 -0600

    fixed blackjack settings

--- Backend/Cycino/src/main/java/coms309/Cycino/GameSettings/BlackJack/BlackJackSettings.java
+++ Backend/Cycino/src/main/java/coms309/Cycino/GameSettings/BlackJack/BlackJackSettings.java
@@ -39,9 +39,13 @@ public class BlackJackSettings {
     public void setUser(User user) {
         this.user = user;
     }
-
+/*
     public User getUser() {
         return user;
+    }
+*/
+    public Long getUserId() {
+        return this.user.getId();
     }
 
     public int getDealerStandOn() {
