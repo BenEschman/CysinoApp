fee927b6ff3c5cafcbc498a4f50c225c48a6e2ec
Evan Litzer <evan@litzer.com>
2024-11-08 21:36:56 -0600

    Got dealerstand, minbet, maxbet, decknumber working completely.

--- Frontend/Cycino/app/src/main/java/com/example/cycino/LeaderboardActivity.java
+++ Frontend/Cycino/app/src/main/java/com/example/cycino/LeaderboardActivity.java
@@ -47,6 +47,7 @@ public class LeaderboardActivity extends AppCompatActivity{
     private TextView titleText;
     RequestQueue requestQueue;
 
+
     int items;
     final int numLines = 2;
     List<String> userNames = new ArrayList<String>();
@@ -256,10 +257,10 @@ public class LeaderboardActivity extends AppCompatActivity{
                     Toast.makeText(getApplicationContext(), "view failed", Toast.LENGTH_LONG).show();
                 }
             });
-
-
         // Add the request to the RequestQueue
         requestQueue.add(jsonObjectRequest);
     }
+
+
 
 }
