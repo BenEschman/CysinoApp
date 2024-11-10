631458e6079d9f902d2f17a2d1824046c2b7698d
filfor <filfor@iastate.edu>
2024-11-09 18:22:00 -0600

    working on dm

--- Backend/Cycino/src/main/java/coms309/Cycino/WebSocketConfig.java
+++ Backend/Cycino/src/main/java/coms309/Cycino/WebSocketConfig.java
@@ -1,24 +1,14 @@
 package coms309.Cycino;
-<<<<<<< HEAD
-=======
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
->>>>>>> 33-blackjack-game-view
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-<<<<<<< HEAD
-//@ConditionalOnProperty(value = "websockets.enabled", havingValue = "true", matchIfMissing = true)
-
-=======
-@ConditionalOnProperty(value = "websockets.enabled", havingValue = "true", matchIfMissing = true)
->>>>>>> 33-blackjack-game-view
 public class WebSocketConfig {
     @Bean
-    public ServerEndpointExporter serverEndpointExporter(){
+    public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
+
 }
