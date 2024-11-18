ecba8c83c76bed2df6283708c81dcc4a8978db1f
filfor <filfor@iastate.edu>
2024-11-17 18:10:49 -0600

    activating both frontend and backend

--- Backend/Cycino/src/main/java/coms309/Cycino/CycinoApplication.java
+++ Backend/Cycino/src/main/java/coms309/Cycino/CycinoApplication.java
@@ -1,5 +1,4 @@
 package coms309.Cycino;
-// Comment for change to get runner activation
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Frontend/Cycino/app/src/main/java/com/example/cycino/AccountSettingsActivity.java
+++ Frontend/Cycino/app/src/main/java/com/example/cycino/AccountSettingsActivity.java
@@ -1,5 +1,5 @@
 package com.example.cycino;
-
+// Comment for activation of runner
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
