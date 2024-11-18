bdacd329d735229657f771bf1ae2a2343feaa7d4
filfor <filfor@iastate.edu>
2024-11-17 18:07:05 -0600

    runner actual activation

--- Backend/Cycino/src/main/java/coms309/Cycino/CycinoApplication.java
+++ Backend/Cycino/src/main/java/coms309/Cycino/CycinoApplication.java
@@ -1,5 +1,5 @@
 package coms309.Cycino;
-
+// Comment for change to get runner activation
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
