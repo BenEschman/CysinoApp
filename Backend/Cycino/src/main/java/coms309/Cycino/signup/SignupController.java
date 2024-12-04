b2d0975653a9e1d717a817278d2ce27f8c7e6a3e
BenEschman <beschman2005@gmail.com>
2024-12-04 11:24:49 -0600

    removed issues

--- Backend/Cycino/src/main/java/coms309/Cycino/signup/SignupController.java
+++ Backend/Cycino/src/main/java/coms309/Cycino/signup/SignupController.java
@@ -1,11 +1,6 @@
 package coms309.Cycino.signup;
 
 import coms309.Cycino.login.LoginInfo;
-import coms309.Cycino.users.User;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,18 +10,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@Api(value = "SignUp Controller", description = "Controller used for signup")
 public class SignupController {
 
 
     @Autowired
 
     private SignupService signupService;
-
-    @ApiOperation(value = "Register user", response = Iterable.class, tags = "signUp")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success|OK"),
-            @ApiResponse(code = 404, message = "not found!!!") })
 
     @PostMapping("/signup/register")
     public Map<String, Object> signup(@RequestBody LoginInfo user){
